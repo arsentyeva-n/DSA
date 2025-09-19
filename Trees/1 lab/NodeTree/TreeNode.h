1f1770c5a69faef327b8ef08108912bcac1e12f4
Terrible <Terrible@DESKTOP-ARHTMMB>
2025-09-19 18:00:06 +0300

    add  Successor

--- Trees/1 lab/NodeTree/TreeNode.h
+++ Trees/1 lab/NodeTree/TreeNode.h
@@ -237,3 +237,211 @@ void BFS(TreeNode<T>* root)
 	}
 }
 
+
+/// Бинарное дерево поиска
+
+//// поиск узла по значению
+//// возвращает уровень, на котором был найден узел
+//// либо -1, если узла с искомым значением нет
+//template <typename T>
+//int SearchNode(TreeNode<T>* root, const T key, int depth = 0) {
+//	// если дошли до nullptr, до искомого значения в дереве нет
+//	if (root == nullptr) {
+//		return -1;
+//	}
+//
+//	// поиск в правом поддереве
+//	if (key > root->Data()) {
+//		SearchNode(root->Right(), key, depth + 1);
+//	}
+//
+//	// поиск в левом поддереве
+//	else if (key < root->Data()) {
+//		SearchNode(root->Left(), key, depth + 1);
+//	}
+//
+//	// если key равен значению в текущем узле
+//	else return depth;
+//}
+//
+//// операция вставки узла в бинарное дерево поиска
+//template <typename T>
+//TreeNode<T>* InsertNode(TreeNode<T>* root, const T value) {
+//
+//	if (root == nullptr) {
+//
+//		// если дерево пустое, то создаём корень (первый узел)
+//		return new TreeNode<T>(value);
+//	}
+//
+//	// Вставляем данные либо в левое, либо в правое поддерево
+//	if (value > root->Data()) {
+//		// если значение, которое нужно добавить, больше текущего
+//		root->SetRight(InsertNode(root->Right(), value));
+//	}
+//	else if (value < root->Data()) {
+//		// если значение, которое нужно добавить, меньше текущего
+//		root->SetLeft(InsertNode(root->Left(), value));
+//	}
+//
+//	//возвращаем корень дерева
+//	return root;
+//}
+
+
+// Нахождение минимального узла в поддереве
+template <typename T>
+TreeNode<T>* minNode(TreeNode<T>* root)
+{
+	while (root->Left() != nullptr) {
+		root = root->Left();
+	}
+
+	return root;
+}
+
+// Нахождение максимального узла в поддереве
+template <typename T>
+TreeNode<T>* maxNode(TreeNode<T>* root)
+{
+	while (root->Right() != nullptr) {
+		root = root->Right();
+	}
+
+	return root;
+}
+
+// Нахождение ближайшего наибольшего
+template <typename T>
+void Successor(TreeNode<T>* root, TreeNode<T>*& succ, int key) {
+
+	// Базовый случай (пустое дерево)
+	if (root == nullptr) {
+		succ = nullptr;
+	}
+
+	// если найден узел, для которого нужно найти ближайшее наибольшее
+	else if (root->Data() == key)
+	{
+		// если есть правый потомок
+		if (root->Right() != nullptr) {
+			// находим самый левый узел от правого потомка
+			succ = minNode(root->Right());
+		}
+	}
+
+	// поиск нужного узла в левом или правом поддереве
+	else if (key < root->Data())
+	{
+		succ = root;
+		Successor(root->Left(), succ, key);
+	}
+	else {
+		Successor(root->Right(), succ, key);
+	}
+}
+
+// операция удаления узла из бинарного дерева поиска
+//template <typename T>
+//TreeNode<T>* DeleteNode(TreeNode<T>* root, const T value) {
+//
+//	// базовый случай
+//	if (root == nullptr) {
+//		return root;
+//	}
+//
+//	// рекурсивный вызов функции, пока не будет найден узел, который нужно удалить
+//	if (root->Data() > value) {
+//		root->SetLeft(DeleteNode(root->Left(), value));
+//		return root;
+//	}
+//	else if (root->Data() < value) {
+//		root->SetRight(DeleteNode(root->Right(), value));
+//		return root;
+//	}
+//
+//	// Когда найден узел на удаление
+//
+//	// Случаи 1 и 2. если есть только 1 потомок или удалить необходимо лист (0 потомков)
+//	// если нет левого потомка, то правый поднимается на место удаляемого узла
+//	if (root->Left() == nullptr) {
+//		TreeNode<T>* temp = root->Right();
+//		delete root;
+//		return temp;
+//	}
+//	// если нет правого потомка, то левый поднимается на место удаляемого узла
+//	else if (root->Right() == nullptr) {
+//		TreeNode<T>* temp = root->Left();
+//		delete root;
+//		return temp;
+//	}
+//
+//	// Случай 3. если есть оба потомка
+//	else {
+//
+//		// узел, который является родителем ближайшего наибольшего
+//		TreeNode<T>* succParent = root;
+//
+//		// Находим ближайшее наибольшее (самый левый узел в правом поддереве) и его родителя
+//		TreeNode<T>* succ = root->Right();
+//		while (succ->Left() != nullptr) {
+//			succParent = succ;
+//			succ = succ->Left();
+//		}
+//
+//
+//		// задача сводится к случаю удаления узла только с правым потомком
+//		// правого потомка ближайшего наибольшего делаем левым потомком родителя
+//		// ближайшего наибольшего
+//
+//		if (succParent != root)
+//			succParent->SetLeft(succ->Right());
+//		else
+//			// если спуска по левому поддереву не было
+//			succParent->SetRight(succ->Right());
+//
+//
+//		// данные из ближайшего наибольшего переносятся на место удаляемого узла
+//		root->SetData(succ->Data());
+//
+//		// Удаляем ближайшее наибольшее
+//		delete succ;
+//		return root;
+//	}
+//}
+//
+//// операция копирования бинарного дерева поиска
+//// возвращает указатель на корень нового дерева
+//template <typename T>
+//TreeNode<T>* CopyTree(TreeNode<T>* t) {
+//
+//	// переменная newnode указывает на новый узел, 
+//	// присоединяемый в дальнейшем
+//	// к новому дереву, указатели newlptr и newrptr адресуют сыновей
+//	// нового узла и передаются в качестве параметров при создании узла
+//	TreeNode<T>* newlptr, * newrptr, * newnode;
+//	// остановить рекурсивное прохождение при достижении пустого дерева
+//	if (t == nullptr)
+//		return nullptr;
+//
+//	// CopyTree строит новое дерево в процессе прохождения узлов дерева t. в каждом
+//	// узле этого дерева функция CopyTree проверяет наличие левого сына, если он
+//	// есть, создается его копия, в противном случае возвращается NULL. CopyTree
+//	// создает копию узла с помощью GetTreeNode и подвешивает к нему копии сыновей.
+//	if (t->Left() != nullptr)
+//		newlptr = CopyTree(t->Left());
+//	else
+//		newlptr = nullptr;
+//	if (t->Right() != nullptr)
+//		newrptr = CopyTree(t->Right());
+//	else
+//		newrptr = nullptr;
+//
+//	// построить новое дерево снизу вверх, сначала создавая
+//	// двух сыновей, а затем их родителя
+//	newnode = new TreeNode<T>(t->Data(), newlptr, newrptr);
+//
+//	// вернуть указатель на вновь созданное дерево
+//	return newnode;
+//
+//}
