32d8312bcf20928072eb613468cad499273ea084
Terrible <marina.vyy@mail.ru>
2025-10-12 20:02:33 +0300

    fix trickleDown, add builHeapBinTree

--- Trees/Heap/Heap.cpp
+++ Trees/Heap/Heap.cpp
@@ -4,17 +4,168 @@
 #include "Heap.h"
 using namespace std;
 
+TreeNode<int>* createTree1() {
+
+
+    //       5
+    //      / \
+    //     4   22
+    //        / \
+    //       12  24
+    //      /
+    //     10
+
+    // Создание узлов
+    TreeNode<int>* root = new TreeNode<int>(5);
+    TreeNode<int>* n1 = new TreeNode<int>(4);
+    TreeNode<int>* n2 = new TreeNode<int>(22);
+    TreeNode<int>* n3 = new TreeNode<int>(12);
+    TreeNode<int>* n4 = new TreeNode<int>(24);
+    TreeNode<int>* n5 = new TreeNode<int>(10);
+
+    root->setLeft(n1);
+    root->setRight(n2);
+
+    n2->setLeft(n3);
+    n2->setRight(n4);
+
+    n3->setLeft(n5);
+
+    return root;
+}
+
+// Вырожденное вправо
+TreeNode<int>* createTree2() {
+
+    //       10
+    //        \
+    //         19
+    //          \
+    //           30
+    //            \
+    //             33
+
+     // Создание узлов
+    TreeNode<int>* root = new TreeNode<int>(10);
+    TreeNode<int>* n1 = new TreeNode<int>(19);
+    TreeNode<int>* n2 = new TreeNode<int>(30);
+    TreeNode<int>* n3 = new TreeNode<int>(33);
+
+    root->setRight(n1);
+    n1->setRight(n2);
+    n2->setRight(n3);
+
+    return root;
+}
+
+// Вырожденное влево
+TreeNode<int>* createTree3() {
+
+    //       60
+    //       /  
+    //      42   
+    //      /   
+    //     14     
+    //     /
+    //    21
+    //    /
+    //   13
+
+
+     // Создание узлов
+    TreeNode<int>* root = new TreeNode<int>(60);
+    TreeNode<int>* n1 = new TreeNode<int>(42);
+    TreeNode<int>* n2 = new TreeNode<int>(14);
+    TreeNode<int>* n3 = new TreeNode<int>(21);
+    TreeNode<int>* n4 = new TreeNode<int>(13);
+
+    root->setLeft(n1);
+    n1->setLeft(n2);
+    n2->setLeft(n3);
+    n3->setLeft(n4);
+
+    return root;
+}
+
+// Завершенное дерево
+TreeNode<int>* createTree4() {
+
+    //          8
+    //       /    \
+    //      4      10
+    //    /   \    / \
+    //   2     7   9   30
+    //  / \    /       
+    // 1   3  5        
+
+     // Создание узлов
+    TreeNode<int>* root = new TreeNode<int>(8);
+    TreeNode<int>* n1 = new TreeNode<int>(4);
+    TreeNode<int>* n2 = new TreeNode<int>(10);
+    TreeNode<int>* n3 = new TreeNode<int>(2);
+    TreeNode<int>* n4 = new TreeNode<int>(7);
+    TreeNode<int>* n5 = new TreeNode<int>(9);
+    TreeNode<int>* n6 = new TreeNode<int>(30);
+    TreeNode<int>* n7 = new TreeNode<int>(1);
+    TreeNode<int>* n8 = new TreeNode<int>(3);
+    TreeNode<int>* n9 = new TreeNode<int>(5);
+
+    // Построение дерева
+    root->setLeft(n1);
+    root->setRight(n2);
+
+    n1->setLeft(n3);
+    n1->setRight(n4);
+
+    n2->setLeft(n5);
+    n2->setRight(n6);
+
+    n3->setLeft(n7);
+    n3->setRight(n8);
+
+    n4->setLeft(n9);
+
+    return root;
+}
+
+
 int main()
 {
+	setlocale(LC_ALL, "RUS");
+
+    // Создаем max-heap
 	Heap<int> h;
-	h.insert(2);
+	h.insert(20);
 	h.insert(10);
 	h.insert(5);
-	h.insert(12);
+	h.insert(15);
+	h.insert(30);
+	h.insert(25);
+	h.insert(100);
+	cout << "\nsize: " << h.Size() << endl;
 	h.printList();
 
+    // Извлекаем максимум и удаляем его
 	cout << "\n" << h.extractMax() << endl;
+	cout << "\nsize: " << h.Size() << endl;
 	h.printList();
-	
+    
+    // Проверка путсоты дерева
+	h.clear();
+	if (h.empty() == true) cout << "\nКуча пустая\n";
+
+    // Создание кучи на основе бинарного дерева
+    TreeNode<int>* root = createTree3();
+    Heap<int> h2 = buildHeapBinTree(root);
+    cout << "\nsize: " << h2.Size() << endl;
+    h2.printList();
+
+    // Удаляем бинарное дерево
+    deleteTree(root);
+
+    // Вставка в кучу на основе бинарного дерева
+    h2.insert(100);
+    cout << "\nsize: " << h2.Size() << endl;
+    h2.printList();
 }
 
--- Trees/Heap/Heap.h
+++ Trees/Heap/Heap.h
@@ -14,7 +14,6 @@ class Heap
 {
 private:
 	vector<T> lst;         // Создаем динамический массив с помощью вектора
-    size_t maxheapsize;    // макс или текущий размер ?? todo:
     size_t heapSize;       // Размер массива
 
     void bubbleUp(int i);       // Функция просеивания вверх
@@ -25,7 +24,7 @@ public:
 	Heap();
 
     // Деструктор
-    //~Heap();
+    ~Heap() { lst.clear(); };
 
     // Возвращает размер кучи
     size_t Size() const;
@@ -34,9 +33,8 @@ public:
     void insert(const T& value);         // Вставка
     void printList();                    // Вывод массива данных
     T extractMax();                      // Удаление корня (максимального элемента в куче) и его извлечение
-    // emty
-    // clear
-    //
+    bool empty() const;                  // Проверка на пустоту
+    void clear();
 
     
 
@@ -47,7 +45,6 @@ template <typename T>
 Heap<T>::Heap() 
 {
     heapSize = lst.size();
-    maxheapsize = lst.size();
 }
 
 
@@ -58,7 +55,20 @@ size_t Heap<T>::Size() const
     return heapSize;
 }
 
-// Просеивание вверх
+// Функция вставки в кучу, О(log n) в худшем, в лучшем О(1)
+template <typename T>
+void Heap<T>::insert(const T& value)
+{
+    // Увеличиваем текущий размер
+    heapSize = heapSize + 1;
+    // Вставляем новый элемент
+    lst.push_back(value);
+    // Восстанавливаем свойство кучи (max-heap)
+    bubbleUp(heapSize - 1);
+}
+
+
+// Просеивание вверх, О(log n) в худшем, в лучшем О(1)
 template <typename T>
 void Heap<T>::bubbleUp(int i)
 {
@@ -71,54 +81,121 @@ void Heap<T>::bubbleUp(int i)
     }
 }
 
-// Просеивание вниз
-template <typename T>
-void Heap<T>::trickleDown(int i)
-{
 
-    int left, right, j;
-    while (2 * i + 1 > heapSize){    // heapSize — количество элементов в куче
-        left = 2 * i + 1;            // left — левый сын
-        right = 2 * i + 2;           // right — правый сын
-        j = left;
-
-        if (right < heapSize && lst[right] < lst[left])
-            j = right;
-
-        if (lst[i] <= lst[j])
-            break;
-
-        swap(lst[i], lst[j]);
-        i = j;
-    } 
-}
-
-// Функция вставки в кучу
-template <typename T>
-void Heap<T>::insert(const T& value)
-{
-    heapSize = heapSize + 1;
-    lst.push_back(value);
-    bubbleUp(heapSize-1);
-}
-
-// Удаление корня (максимального элемента в куче) и его извлечение TODO
+// Удаление корня (максимального элемента в куче) и его извлечение, О(log n) в худшем, 
+// в лучшем О(1) возможно только, если последний элемент больше всех остальных
 template <typename T>
 T Heap<T>::extractMax()
 {
+    // Запоминаем max
     T min = lst[0];
+    
+    // На его место ставим самый правый лист дерева (последний элемент)
     lst[0] = lst[heapSize - 1];
+    // Уменьшаем размер массива
     heapSize = heapSize - 1;
+    // Восстанавливаем свойство кучи просеиванием вниз
     trickleDown(0);
+    // Удаляем лишний элемент в массиве
+    lst.pop_back();
+
+    //Возвращаем максимум
     return min;
+}
+
+
+// Просеивание вниз, О(log n) в худшем, в лучшем О(1)
+template <typename T>
+void Heap<T>::trickleDown(int i)
+{
+    // Инициалищируем дополнительные переменные
+    int left, right, j;
+
+    //cout << lst[i];
+    // Сравниваем корень с потомками, пока не кончится дерево
+    while (2 * i + 1 < heapSize) {    // heapSize — количество элементов в куче
+        left = 2 * i + 1;             // left — левый сын
+        right = 2 * i + 2;            // right — правый сын
+        
+        // Если правый сын больше
+        if (lst[i] < lst[right])
+            // Запоминаем его индекс
+            j = right;
+
+        // Если левый сын больше
+        else if (lst[i] < lst[left])
+            // Запоминаем его индекс
+            j = left;
+        
+        // Если текущий элемент больше или равен, то цикл заканчивается
+        if (lst[i] >= lst[j])
+            break;
+
+        // Иначе меняем местами корень и потомка
+        swap(lst[i], lst[j]);
+        // Дальше просеиваем корень с его потомками
+        i = j;
+    }
+}
+
+
+// Проверка на пустоту
+template<typename T>
+bool Heap<T>::empty() const {
+    return lst.empty();
+}
+
+
+// Очистка массива
+template<typename T>
+void Heap<T>::clear() {
+    lst.clear();
+    heapSize = 0;
 }
 
 
 // Вывод массива данных
 template<typename T>
 void Heap<T>::printList() {
-    for (const auto& element : this->lst) {
+    for (const auto& element : lst) {
         cout << element << " ";
     }
     cout << endl;
+}
+
+
+// Создание кучи на основе бинарного дерева
+template <typename T>
+Heap<T> buildHeapBinTree(TreeNode<T>* root)
+{
+    Heap<T> heap;
+    // Если дерево не пустое
+    if (root != nullptr) 
+    {
+        
+
+        // Создаем очередь
+        queue <TreeNode<T>*> q;
+
+        // Сохраняем корень в очередь
+        q.push(root);
+
+        // Пока очередь не пустая
+        while (!q.empty()) 
+        {
+            // Извлекаем первый элемент из очереди и добавляем в кучу
+            TreeNode<T>* node = q.front();
+            heap.insert(node->Data());
+
+            // Удаляем текущий узел и переходим к следующим
+            q.pop();                    
+
+            // Добавляем в очередь левого и правого потомка (если существуют)
+            if (node->Left() != nullptr)
+                q.push(node->Left());
+            if (node->Right() != nullptr)
+                q.push(node->Right());
+        }
+    }
+    return heap;
 }
